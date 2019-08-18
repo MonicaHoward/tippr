d6f651e3fe6b3f6312d67c8f0c81228ae559ee3b
Monica Howard <jay.m.howard@gmail.com>
2019-08-18 18:32:13 -0500

    updates to js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 let tipOutput = document.querySelector(".tip-output");
 let billAmount = document.querySelector(".bill-amount");
 let percent10 = document.querySelector(".percent-10");
+let tipAmount = 0;
+
+let calculateTip = () => {
+  console.log("click");
+  tipAmount = billAmount.value * 0.1;
+  console.log(tipAmount);
+  tipOutput.innerHTML = tipAmount;
+  billAmount.value = "";
+};
+
+percent10.addEventListener("click", calculateTip);
